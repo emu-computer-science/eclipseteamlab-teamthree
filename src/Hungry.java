d2ad4e21712995a01783d8cfa679e89ce419ff93
gsingh16-cyber <gsingh16@emich.edu>
2026-02-16 17:48:36 -0500

    Thrid commit

--- src/Hungry.java
+++ src/Hungry.java
@@ -9,5 +9,7 @@ public class Hungry {
 	public static void hungry() {
 	System.out.println("We are feeling hungry");
 	}
-
+	public static void happy( ) {
+	System.out.println("I am really hungry rn");
+	}
 }
