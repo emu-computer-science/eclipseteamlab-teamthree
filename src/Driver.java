ab072e6f12b70f21e2d0772d0718b31c837c8d13
gsingh16-cyber <gsingh16@emich.edu>
2026-02-16 17:32:06 -0500

    Second commit

--- src/Driver.java
+++ src/Driver.java
@@ -4,6 +4,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println ("Our team is getting started");
+	Hungry.hungry();
 	}
 
 }
---
+++ src/Hungry.java
@@ -0,0 +1,13 @@
+
+public class Hungry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void hungry() {
+	System.out.println("We are feeling hungry");
+	}
+
+}
